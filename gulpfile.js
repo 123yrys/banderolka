7603eba9f1443147787d224bfb69e7142b3ac250
Aziret Suiuntbek Uulu <aziretsuiuntbek@gmail.com>
2019-02-06 13:33:00 +0600

    footer

--- gulpfile.js
+++ gulpfile.js
@@ -8,46 +8,46 @@ var gulpCopy = require('gulp-copy');
 const SCSS_FOLDER = "./assets/style/**/*.scss";
 const TEMPALTES_FOLDER = "./templates/**/*.html";
 
-gulp.task("sass", function (cb) {
-  return gulp
+gulp.task("sass", function(cb) {
+    return gulp
 
-    .src("./scss/style.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./build/css"))
-    .pipe(browserSync.stream());
+        .src("./scss/style.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./build/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task("nunjucks", function (cb) {
-  gulp
-    .src("./templates/*.nj")
-    .pipe(nunjucks({
-      searchPaths: ['./templates/chunks', './templates/layout'],
-      ext: '.html'
-    }))
-    .pipe(gulp.dest("./build"))
-  browserSync.reload();
-  cb()
+gulp.task("nunjucks", function(cb) {
+    gulp
+        .src("./templates/*.nj")
+        .pipe(nunjucks({
+            searchPaths: ['./templates/chunks', './templates/layout'],
+            ext: '.html'
+        }))
+        .pipe(gulp.dest("./build"))
+    browserSync.reload();
+    cb()
 });
 
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./build"
-    }
-  });
-  gulp.watch('./scss/**/*.scss', gulp.parallel('sass'))
-  gulp.watch('./templates/**/*.nj', gulp.parallel('nunjucks'))
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+    gulp.watch('./scss/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('./templates/**/*.nj', gulp.parallel('nunjucks'))
 });
 
-gulp.task('gulp-copy-img', function () {
-  return gulp
-    .src(['./assets/images/**'])
-    .pipe(gulp.dest('./build/assets/images'));
+gulp.task('gulp-copy-img', function() {
+    return gulp
+        .src(['./assets/images/**'])
+        .pipe(gulp.dest('./build/assets/images'));
 })
-gulp.task('gulp-copy-fonts', function () {
-  return gulp
-    .src(['./assets/fonts/**'])
-    .pipe(gulp.dest('./build/assets/fonts'));
+gulp.task('gulp-copy-fonts', function() {
+    return gulp
+        .src(['./assets/fonts/**'])
+        .pipe(gulp.dest('./build/assets/fonts'));
 })
 
 // gulp.task(
@@ -57,4 +57,3 @@ gulp.task('gulp-copy-fonts', function () {
 //     gulp.parallel("gulp-copy", "sass", "nunjucks", "browser-sync")
 //   )
 // );
-
